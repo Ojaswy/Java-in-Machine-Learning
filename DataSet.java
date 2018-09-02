8bd0e2ec557e3d9ea234d582474dbfef30930a60
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-09-02 14:08:30 +0530

    Update DataSet.java

--- DataSet.java
+++ DataSet.java
@@ -16,7 +16,7 @@ public class DataSet {
 		
 	  try{  
 	    FileInputStream fin=new FileInputStream("src/adult.txt"); 
-	   
+	     //Construct BufferedReader from InputStreamReader
 		BufferedReader br = new BufferedReader(new InputStreamReader(fin));
 		String line = null;
 		
@@ -41,7 +41,7 @@ public class DataSet {
 	 }
 	/*
 	 * A method to put each line in ArrayList of strings from input test file.
-	 * @return ArrayList of Strings with each element as line of file.
+	 
 	 */
 public static ArrayList<String> getInstance()
 {
